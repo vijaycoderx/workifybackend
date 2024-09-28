e60172d8b150d9b72af4c4887eb1fd28c2410c3e
K.VIJAY KUMAR <vijaycoderx@gmail.com>
2024-09-28 11:35:47 +0530

    final server update

--- controllers/jwtUtils.js
+++ controllers/jwtUtils.js
@@ -2,21 +2,24 @@ import jwt from "jsonwebtoken";
 
 const createJWT = (data) => {
     try {
-        const generateJWT = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: "1h" });
+        const generateJWT = jwt.sign(data, process.env.JWT_SECRET, { expiresIn: "24h" });
         return generateJWT;
     } catch (error) {
         return error;
     }
 }
 
-const verifyJWT = (data, secret) => {
+const verifyJWT = (data) => {
     try {
-        const verifiedJWT = jwt.verify(data, secret);
+        const verifiedJWT = jwt.verify(data, process.env.JWT_SECRET);
+        // console.log("jwt data" , verifiedJWT);
         return verifiedJWT;
     } catch (error) {
         return error;
     }
 }
+
+export default { createJWT, verifyJWT };
 
 
 
--- routes/googleAuth.js
+++ routes/googleAuth.js
@@ -1,49 +1,86 @@
 import express from "express";
-import dotenv from "dotenv"
-import { OAuth2Client } from "google-auth-library";
+import userDataService from "../services/userDataService.js";
+import bcrypt from "bcryptjs";
+// import dotenv from "dotenv"
+// import { OAuth2Client } from "google-auth-library";
+
+import googleAccess from "../controllers/googleAccess.js"
+import jwtUtils from "../controllers/jwtUtils.js";
 
 const router = express.Router();
-const dotenvConfig = dotenv.config();
+// const dotenvConfig = dotenv.config();
 
 router.get("/", (req, res) => {
     res.send("Auth HomePage")
 })
 
-router.post("/google", (req, res) => {
-    res.header('Access-Control-Allow-Origin', process.env.FRONT_END_ORIGIN);
+router.post("/google/signup", (req, res) => {
+    res.header('Access-Control-Allow-Origin', process.env.FRONTEND_ORIGIN);
     res.header('Referrer-Policy', 'no-referrer-when-downgrade');
-
-    const oAuth2Client = new OAuth2Client(
-        process.env.CLIENT_ID,
-        process.env.CLIENT_SECRET,
-        process.env.GOOGLE_CALLBACK
-    )
-    const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: "offline",
-        scope: "https://www.googleapis.com/auth/userinfo.profile openid",
-        prompt: "consent",
-    })
-
-    res.redirect(authUrl)
+    const generatedUrl = googleAccess.googleUrlGenerator(req, res);
+    console.log("google" + generatedUrl + "\n");
+    // res.redirect(generatedUrl);
+    res.json({generatedUrl});
+    
+    
 })
 
-router.get("/googleAuthorized", async (req, res) => {
-    const authorizedCode = req.query.code;
+router.get("/googleAuthorizedCallback", async (req, res) => {
+    // res.header('Access-Control-Allow-Origin', process.env.FRONTEND_ORIGIN);
+    // res.header('Referrer-Policy', 'no-referrer-when-downgrade');
+    const userTokens = await googleAccess.googleAuthorizedGenerateTokens(req, res);
+    const userData = await googleAccess.userInfo(userTokens.access_token);
+    console.log("USERDATA", userData);
+    
+    const salt = await bcrypt.genSalt(10);
+    const ptokenStr = Math.random().toString(36) + Math.random() * 1000000000 + Math.random().toString(36);
+    let hashedCode = await bcrypt.hash(ptokenStr, salt);
+    
+    const userDataForDB = {
+        email: userData.email,
+        name: userData.name ? userData.name : "",
+        username: userData.username ? userData.username : undefined,
+        photo: userData.picture ? userData.picture : "",
+        accountType: "google",
+        ptoken: hashedCode,
+        password: hashedCode,
 
-    try {
-        const oAuth2Client = new OAuth2Client(
-            process.env.CLIENT_ID,
-            process.env.CLIENT_SECRET,
-            process.env.GOOGLE_CALLBACK
-        )
-        
-        const getTokens = await oAuth2Client.getToken(authorizedCode);
-        const oAuth2ClientCredentials = await oAuth2Client.setCredentials(getTokens.tokens);
-        const user = oAuth2ClientCredentials.credentials;
-
-    } catch (error) {
-        console.log("error is: " + error);
     }
+    const addUserRespose = await userDataService.addUser(userDataForDB);
+    console.log("USERRESPONSE",userDataForDB)
+    if (addUserRespose) {
+        console.log(addUserRespose)
+        const modifyAddUserRespose = JSON.parse(JSON.stringify(addUserRespose));
+        modifyAddUserRespose.id = modifyAddUserRespose._id.toString();
+        delete modifyAddUserRespose.__v;
+        delete modifyAddUserRespose._id;
+        
+        const userJWTToken = jwtUtils.createJWT(modifyAddUserRespose);
+        res.redirect(`${process.env.FRONTEND_ORIGIN}/admin?userJWTToken=${userJWTToken}`);
+
+    } else {
+        const userDataForFetch = {
+            email: userData.email,
+        }
+        const userDataRes = await userDataService.userData(userDataForFetch);
+        console.log("fetched data", userDataRes)
+        const modifyAddUserRespose = JSON.parse(JSON.stringify(userDataRes));
+        modifyAddUserRespose.id = modifyAddUserRespose._id.toString();
+        delete modifyAddUserRespose.__v;
+        delete modifyAddUserRespose._id;
+        const userJWTToken = jwtUtils.createJWT(modifyAddUserRespose);
+        res.redirect(`${process.env.FRONTEND_ORIGIN}/admin?userJWTToken=${userJWTToken}`);
+        // res.redirect(`${process.env.FRONTEND_ORIGIN}/auth`);
+    }
+    
+   
+
+    // const userJWTToken = jwtUtils.createJWT(modifyAddUserRespose);
+
+    // res.redirect(`${process.env.FRONTEND_ORIGIN}/admin?userJWTToken=${userJWTToken}`);
+
+    // res.redirect(`${process.env.FRONTEND_ORIGIN}/admin?userJWTToken=hi`);
+
 })
 
 
